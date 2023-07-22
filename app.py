b5f476ddf1add4bc2a134a8671a87e92b49d5992
andy-minn <99483042+andy-minn@users.noreply.github.com>
2023-07-22 01:03:36 +0000

    convert to dictionary bug fixed

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from database import load_jobs_from_db
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello_jovian():
   jobs = load_jobs_from_db()
   return render_template('home.html', jobs=jobs, company_name='Jovian')
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine, text
 
-db_conn_str = "mysql+pymysql://4800c59u2iigppk8vlrp:pscale_pw_iocxD5trQZMZD2QBpfGkkfT0K1dPsRuFqkcZmN37jxq@aws.connect.psdb.cloud/joviancareers"
+db_conn_str = "mysql+pymysql://xiqvi2ps6xtblut22w1j:pscale_pw_nDAwEchf9iAh44vDbfDG8v2ErKGqU02ojBNXtPFC29L@aws.connect.psdb.cloud/joviancareers?charset=utf8mb4"
 
 engine = create_engine(
   db_conn_str,
   connect_args={
-    "ssl":{
+    "ssl": {
       "ssl_ca": "/etc/ssl/cert.pem"
     }
   })
@@ -15,5 +15,5 @@ def load_jobs_from_db():
     result = conn.execute(text("select * from jobs"))
     jobs = []
     for row in result.all():
-      jobs.append(dict(row))
+      jobs.append(dict(row._mapping))
     return jobs
